a7a9ef7a4f360f02b0ca257b6c8ceb8148a500e4
Talor.Hammond <talor.hammond@kiwiwealth.co.nz>
2021-12-06 09:36:22 +1300

    Add User class

---
+++ lib/models/user.dart
@@ -0,0 +1,120 @@
+class User {
+  late int id;
+  late String name;
+  late String username;
+  late String email;
+  late Address address;
+  late String phone;
+  late String website;
+  late Company company;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.username,
+      required this.email,
+      required this.address,
+      required this.phone,
+      required this.website,
+      required this.company});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address =
+        (json['address'] != null ? Address.fromJson(json['address']) : null)!;
+    phone = json['phone'];
+    website = json['website'];
+    company =
+        (json['company'] != null ? Company.fromJson(json['company']) : null)!;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['username'] = username;
+    data['email'] = email;
+    data['address'] = address.toJson();
+    data['phone'] = phone;
+    data['website'] = website;
+    data['company'] = company.toJson();
+    return data;
+  }
+}
+
+class Address {
+  late String street;
+  late String suite;
+  late String city;
+  late String zipcode;
+  late Geo geo;
+
+  Address(
+      {required this.street,
+      required this.suite,
+      required this.city,
+      required this.zipcode,
+      required this.geo});
+
+  Address.fromJson(Map<String, dynamic> json) {
+    street = json['street'];
+    suite = json['suite'];
+    city = json['city'];
+    zipcode = json['zipcode'];
+    geo = (json['geo'] != null ? Geo.fromJson(json['geo']) : null)!;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['street'] = street;
+    data['suite'] = suite;
+    data['city'] = city;
+    data['zipcode'] = zipcode;
+    data['geo'] = geo.toJson();
+    return data;
+  }
+}
+
+class Geo {
+  late String lat;
+  late String lng;
+
+  Geo({required this.lat, required this.lng});
+
+  Geo.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['lat'] = lat;
+    data['lng'] = lng;
+    return data;
+  }
+}
+
+class Company {
+  late String name;
+  late String catchPhrase;
+  late String bs;
+
+  Company({required this.name, required this.catchPhrase, required this.bs});
+
+  Company.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    catchPhrase = json['catchPhrase'];
+    bs = json['bs'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['catchPhrase'] = catchPhrase;
+    data['bs'] = bs;
+    return data;
+  }
+}
