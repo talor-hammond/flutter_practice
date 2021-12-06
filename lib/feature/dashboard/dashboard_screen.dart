f59c2c5bcbd6efab1dad0dee962892387bd2eb0b
Talor.Hammond <talor.hammond@kiwiwealth.co.nz>
2021-12-06 15:48:36 +1300

    Display user name on Dashboard screen

--- lib/feature/dashboard/dashboard_screen.dart
+++ lib/feature/dashboard/dashboard_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/feature/dashboard/dashboard_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends ConsumerWidget {
+  static const route = "/";
+
+  static PageRoute<void> create(RouteSettings routeSettings) {
+    return MaterialPageRoute(builder: (context) {
+      return const DashboardScreen();
+    });
+  }
+
   const DashboardScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    var dashboard = ref.watch(dashboardModel);
+
+    return Scaffold(
+      body: Builder(builder: (context) {
+        if (dashboard.isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (dashboard.isError) {
+          return const Center(child: Text("Oops"));
+        }
+
+        return Center(child: Text("Welcome ${dashboard.user.value?.name}"));
+      }),
+    );
   }
 }
